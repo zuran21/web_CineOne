65cc81cbfaca7e4ae882fb93435ce06b7af74cbb
ashley zifrikc villanueva <72785236@certus.edu.pe>
2022-07-09 19:09:18 -0500

    web1

---
+++ index.js
@@ -0,0 +1,8 @@
+  // Slicknav menu
+  $('.navbar-nav').slicknav({
+    allowParentLinks: true,
+    label: "",
+    appendTo: "#masthead",
+    closedSymbol: '<i class="fa fa-caret-down" aria-hidden="true"></i>',
+    openedSymbol :'<i class="fa fa-caret-up" aria-hidden="true"></i>'
+});
